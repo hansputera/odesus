3080e1d14de0a4d070d275a3337ea27e9c020bdb
Yao-i5 <128717815+yaoiiiii@users.noreply.github.com>
2023-04-03 15:41:14 +0000

    feat: add cheerio, and initialize odesus class
    Signed-off-by: Yao-i5 <128717815+yaoiiiii@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-export const odesus = () => 'sus';
+import gaxios from 'gaxios';
+
+/**
+ * @class Odesus
+ */
+export class Odesus {
+	protected client!: gaxios.Gaxios;
+	/**
+     * @constructor
+     * @param baseUrl Otakudesu Base URL
+     */
+	constructor(baseUrl = 'https://otakudesu.lol') {
+		gaxios.instance.defaults = {
+			baseUrl,
+			headers: {
+				'User-Agent': 'Odesus/1.0',
+			},
+		};
+
+		this.client = gaxios.instance;
+	}
+}
